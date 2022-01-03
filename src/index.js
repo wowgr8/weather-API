4dafc28c74c08f59c2937afc2f9e911fa6cda559
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-01-03 12:05:43 -0800

    secure API key

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
